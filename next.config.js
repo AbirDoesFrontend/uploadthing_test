c7ead5ef9be959bf07fe63e8ed0224a9d8003a50
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-11-28 06:08:01 +0600

    nextjs-upload-image

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "uploadthing.com",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+      "images.unsplash.com",
+      "icon-library.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
